f777575ecd22c009f4a19fd9d6644277ef36cb46
Jihun Ahn <jhun.ahnn@gmail.com>
2025-04-13 14:15:03 +0900

    Modify to apply eslint
    Signed-off-by: Jihun Ahn <jhun.ahnn@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import vue from "@vitejs/plugin-vue";
 import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+// @ts-expect-error: type issues with vite-plugin-eslint
+import eslint from "vite-plugin-eslint";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/getting-married/',
-  plugins: [vue()],
+  base: "/getting-married/",
+  plugins: [vue(), eslint()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
