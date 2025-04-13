3d5fc6706e1f3791b69ea15435417f8e58a9bbf2
Jihun Ahn <jhun.ahnn@gmail.com>
2025-04-13 14:15:03 +0900

    Modify to apply eslint
    Signed-off-by: Jihun Ahn <jhun.ahnn@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,24 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import WelcomeSection from './components/WelcomeSection.vue'
-import CoupleInfo from './components/CoupleInfo.vue'
-import LocationSection from './components/LocationSection.vue'
+import { defineComponent } from "vue";
+
+import CoupleInfo from "./components/CoupleInfo.vue";
+import LocationSection from "./components/LocationSection.vue";
+import WelcomeSection from "./components/WelcomeSection.vue";
 
 export default defineComponent({
   components: {
     WelcomeSection,
     CoupleInfo,
-    LocationSection
-  }
-})
+    LocationSection,
+  },
+});
 </script>
 
 <style scoped>
 .app {
-  font-family: 'Noto Sans KR', sans-serif;
+  font-family: "Noto Sans KR", sans-serif;
   color: #333;
   padding: 20px;
 }
--- src/components/LocationSection.vue
+++ src/components/LocationSection.vue
@@ -1,26 +1,26 @@
 <template>
-    <section class="location">
-      <h4>오시는 길</h4>
-      <iframe
-        src="https://map.kakao.com/?q=W스퀘어컨벤션"
-        width="100%"
-        height="250"
-        style="border:0;"
-      ></iframe>
-    </section>
-  </template>
-  
-  <script lang="ts">
-  export default {}
-  </script>
-  
-  <style scoped>
-  .location {
-    margin-top: 40px;
-    text-align: center;
-  }
-  h4 {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-  </style>
+  <section class="location">
+    <h4>오시는 길</h4>
+    <iframe
+      src="https://map.kakao.com/?q=W스퀘어컨벤션"
+      width="100%"
+      height="250"
+      style="border: 0"
+    ></iframe>
+  </section>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style scoped>
+.location {
+  margin-top: 40px;
+  text-align: center;
+}
+h4 {
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+</style>
