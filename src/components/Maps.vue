f117f65d2ddc84659c243f53a97f4baae548924e
Jihun Ahn <jhun.ahnn@gmail.com>
2025-05-05 20:57:40 +0900

    Modify to add maps section
    Signed-off-by: Jihun Ahn <jhun.ahnn@gmail.com>

---
+++ src/components/Maps.vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="maps">
+    <ContentsTitle title="Location" />
+    <div class="info text-int">
+      <strong class="tt">{{ location.name }}</strong>
+      <span class="tt">{{ location.hall }}</span>
+    </div>
+    <div class="map-wrap text-int">
+      <KakaoMap
+        class="tt"
+        :lat="location.detail.coordinate.base[0]"
+        :lng="location.detail.coordinate.base[1]"
+        :draggable="true"
+        width="auto"
+        height="400px"
+      >
+        <KakaoMapMarker
+          :lat="location.detail.coordinate.base[0]"
+          :lng="location.detail.coordinate.base[1]"
+        />
+        <KakaoMapMarker
+          v-for="(item, key) in location.detail.coordinate.references"
+          :key="key"
+          :lat="item[0]"
+          :lng="item[1]"
+          :image="{
+            imageSrc:
+              'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
+            imageWidth: 22,
+            imageHeight: 22,
+            imageOption: {
+              alt: String(key),
+            },
+          }"
+        />
+      </KakaoMap>
+      <ul class="way text-int">
+        <li class="tt short">
+          <dl>
+            <dt>자차</dt>
+            <dd>
+              <div>
+                <span
+                  v-for="(item, idx) in location.detail.transfortation.car
+                    .address"
+                  :key="idx"
+                  class="tt"
+                  >{{ item }}</span
+                >
+              </div>
+            </dd>
+          </dl>
+        </li>
+      </ul>
+      <ul class="way text-int">
+        <li class="tt short">
+          <dl>
+            <dt>지하철</dt>
+            <dd>
+              <span
+                v-for="(item, idx) in location.detail.transfortation.subway
+                  .desc"
+                :key="idx"
+                class="tt"
+                >{{ item }}</span
+              >
+            </dd>
+          </dl>
+        </li>
+      </ul>
+      <ul class="way text-int">
+        <li class="tt short">
+          <dl>
+            <dt>버스</dt>
+            <dd>
+              <li
+                v-for="(item, key) in location.detail.transfortation.bus"
+                :key="key"
+              >
+                {{ key }}: {{ item }}
+              </li>
+            </dd>
+          </dl>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+import { KakaoMap, KakaoMapMarker } from "vue3-kakao-maps";
+
+import ContentsTitle from "@/components/ContentsTitle.vue";
+
+type LocationInformation = {
+  name: string;
+  city: string;
+  hall: string;
+  detail: {
+    coordinate: {
+      base: number[];
+      references: {
+        [key: string]: number[];
+      };
+    };
+    transfortation: {
+      car: {
+        address: string[];
+      };
+      subway: {
+        lines: string[];
+        desc: string[];
+      };
+      bus: {
+        [key: string]: string;
+      };
+    };
+  };
+};
+
+defineProps<{
+  location: LocationInformation;
+}>();
+</script>
+
+<style lang="scss">
+@import "@/assets/css/reset.scss";
+@import "@/assets/css/variables.scss";
+@import "@/assets/css/common.scss";
+
+.maps {
+  position: relative;
+  margin-top: #{$top-gap-2x}px;
+  color: $col-key;
+}
+.info {
+  position: relative;
+  z-index: $z-bg;
+  font-size: $font-xs;
+  line-height: 1.4;
+
+  margin-top: 20px;
+  strong {
+    font-weight: bold;
+  }
+  span {
+    margin-top: 10px;
+    font-size: $font-xxs;
+  }
+}
+.map-wrap {
+  position: relative;
+  margin: 0 -#{$side-padding};
+  z-index: $z-bg;
+
+  &.mapFixed {
+    position: relative;
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: 1;
+    }
+    svg {
+      display: none;
+    }
+  }
+}
+#map {
+  left: #{-$side-padding};
+  background-color: pink;
+}
+.way {
+  margin: 32px 0 0 20px;
+  > li {
+    margin-top: 22px;
+    font-size: $font-xxs;
+    line-height: 1.6;
+    dl {
+      display: flex;
+      dt {
+        width: 40rem;
+      }
+      dd {
+        width: calc(100% - 40rem);
+      }
+    }
+  }
+}
+</style>
